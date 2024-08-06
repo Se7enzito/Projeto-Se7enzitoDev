7a7103834df33fb501b32be58fcb02e33799b198
Se7enzito <bernardocastro2006@gmail.com>
2024-08-05 22:46:36 -0300

    Atualizações código: Inicio Dashboard; Painel Gerenciamento; Login/Registro

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, session
 from flask_session import Session
 
 app = Flask(__name__, template_folder="../frontend/templates")
@@ -14,21 +14,60 @@ Session(app)
 def index():
     return render_template('index.html', idade = 17)
     
-@app.route('/entrar')
-def entrar():
-    pass
-
-@app.route('/logout')
-def logout():
-    pass
-
-@app.route('/dashboard')
-def cadastro():
-    pass
-
 @app.route('/login')
 def login():
+    user = session.get('user')
+    senha = session.get('senha')
+    
+    if user == None or senha == None:
+        return render_template('login.html')
+    
+    return render_template('dashboard.html', user=user)
+    
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    session.pop('senha', None)
+    
+    return redirect(url_for('index'))
+
+@app.route('/dashboard')
+def dashboard():
+    user = session.get('user')
+    
+    if user == None:
+        return redirect(url_for('login'))
+    
+    return render_template('dashboard.html', user=user)
+
+@app.route('/contratar')
+def contratar():
+    user = session.get('user')
+    
+    if user == None:
+        pass
+    
     pass
+
+@app.route('/saber_mais')
+def saber_mais():
+    user = session.get('user')
+    
+    if user == None:
+        pass
+    
+    pass
+
+@app.route('/gerenciamento')
+def gerenciamento():
+    user = session.get('user')
+    
+    if user == 'Se7enzitoDev':
+        return render_template('gerenciamento.html', user=user)
+    
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
+    
+    # EmailJS para conectar com e-mail
